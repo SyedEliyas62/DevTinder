73ec3024ee3be0cadc58661a02016bb6b4ac1195
SyedEliyas62 <syedeliyas62@gmail.com>
2025-02-24 12:07:57 +0530

    Exploring get, post, delete

--- scr/app.js
+++ scr/app.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 
 
-app.use("/", (req,res) =>{
-    res.send("Hello world from india");
+app.get("/user", (req,res) =>{
+    res.send( {firstName:"Syed", lastName: "Eliyas"});
 });
 
-app.listen(3000);
+app.post("/user", (req,res) =>{
+    console.log(req.body);
+    res.send("Data sucessfully saved to the database");
+});
+
+app.delete("/user", (req,res) => { 
+    res.send("Deleted sucessfully");
+})
+
+app.use("/test", (req,res)=>{
+    res.send("Hello from the sever.")
+})
+
+
+app.listen(7777, () =>{
+    console.log("Server is successfully listerning on port 7777...")
+});
