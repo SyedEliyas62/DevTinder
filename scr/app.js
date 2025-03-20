2a286f7d9d63e26185f412b51f954c3fac4a93f6
SyedEliyas62 <syedeliyas62@gmail.com>
2025-03-20 16:41:42 +0530

    Authentication, JSONWEBTOKEN, and Cookies

--- scr/app.js
+++ scr/app.js
@@ -2,58 +2,32 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-const {validateSignUpData} = require("./utils/validation")
+const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
+const user = require("./models/user");
 
 app.use(express.json());
-
-//Get user by email
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-
-  try {
-    const user = await User.findOne({ emailId: userEmail });
-    res.send(user);
-    // const users  = await User.find({emailId : userEmail})
-    // if(users.length === 0){
-    //     res.status(404).send("User not wrong");
-    // }else{
-    //     res.send(users);
-    // }
-  } catch (error) {
-    res.status(400).send("Someing want wrong.");
-  }
-});
-
-// Feed API -GET/feed - get all the users from the databases
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(400).send("someting want wrong");
-  }
-});
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-
-
   try {
-      // Validation of data
-  validateSignUpData(req);
+    // Validation of data
+    validateSignUpData(req);
 
-  const {firstName,lastName,emailId, password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
 
-  const passwordHash = await bcrypt.hash(password,10);
-  console.log(passwordHash);
-  //Creating a new instance of the User model
-  const user = new User({
-    firstName,
-    lastName,
-    emailId,
-    password : passwordHash,
-  });
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    //Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.send("User added Successfully!");
   } catch (err) {
@@ -61,71 +35,43 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
-app.post("/login", async (req,res) => {
+app.post("/login", async (req, res) => {
   try {
-    const {emailId, password} = req.body;
-    const user = await User.findOne({emailId: emailId});
-    if(!user){
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
       throw new Error("EmailId is not present in DB");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      const token = await user.getJWT();
 
-    if(isPasswordValid){
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 1 * 3600000),
+      });
       res.send("Login Sucessfully!!");
-    }else{
+    } else {
       throw new Error("Passwors is not correct");
     }
-
   } catch (err) {
     res.status(400).send("ERROR :" + err.message);
-    
   }
 });
 
-
-
-
-
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(userId);
-    res.send("user deleted Sucessfully.");
-  } catch (error) {
-    res.status(400).send("Something want wrong.");
-  }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  
-  try {
-    const ALLOWED_UPDATE = ["photoUrl", "about", "gender", "age", "skills"];
-
-  const isUpdatedAllowed = Object.keys(data).every((k) =>
-    ALLOWED_UPDATE.includes(k)
-  );
-  if(!isUpdatedAllowed){
-    throw new Error("Update not allowed");
-  }
-  if(data?.skills.length > 10){
-    throw new Error("Skills cnnot be more than 10.")
-  }
-
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "before",
-      runValidators:true,
-      
-    });
-    console.log(user);
-    res.send("User updated sucessfully");
+    const user = req.user;
+    res.send(user);
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("ERROR : " + err.message);
   }
+});
+
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  console.log("Sending a connection request");
+
+  res.send(user.lastName + " Sent a connection request");
 });
 
 connectDB()
--- scr/models/user.js
+++ scr/models/user.js
@@ -1,72 +1,94 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const validator = require("validator");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new Schema({
-  firstName: {
-    type : String,
-    required: true,
-    minLength : 4,
-    maxLength : 50,
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid eamil address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://pinnacle.works/wp-content/uploads/2022/06/dummy-image.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo Url address: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName : {
-    type : String,
-    required: true,
-  },
-  emailId : {
-    type : String,
-    lowercase:true,
-    required: true,
-    unique:true,
-    trim:true,
-    validate(value){
-      if(!validator.isEmail(value)){
-        throw new Error("Invalid eamil address: " + value);
-      }
-    }
-  },
-  password : {
-    type : String,
-    required: true,
-    validate(value){
-      if(!validator.isStrongPassword(value)){
-        throw new Error("Enter a Strong Password: " + value);
-      }
-    }
-  },
-  age : {
-    type : Number,
-    min:18,
-    
-  },
-  gender : {
-    type : String,
-    validate(value){
-      if(!["male","female", "others"].includes(value)){
-        throw new Error("Gender data is not valid");
-      }
-    }
-  },
-  photoUrl : {
-    type : String,
-    default:"https://pinnacle.works/wp-content/uploads/2022/06/dummy-image.jpg",
-    validate(value){
-      if(!validator.isURL(value)){
-        throw new Error("Invalid Photo Url address: " + value);
-      }
-    }
-  },
-  about : {
-    type: String,
-    default : "This is a default about of the user!"
-  },
-  skills : {
-    type:[String]
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps:true,
-});
+);
 
-module.exports = mongoose.model("User",UserSchema);
+UserSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "DEV@inder$798", {
+    expiresIn: "7d",
+  });
 
+  return token;
+};
+
+UserSchema.methods.validatePassword = async function (passwordInputUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputUser,
+     passwordHash
+    );
+    return isPasswordValid;
+};
+
+module.exports = mongoose.model("User", UserSchema);
