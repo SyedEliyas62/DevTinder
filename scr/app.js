f506f7b41aaba6d21f544f016927165f0b78b0c6
SyedEliyas62 <syedeliyas62@gmail.com>
2025-03-24 20:26:31 +0530

    Ref, populate used in API

--- scr/app.js
+++ scr/app.js
@@ -3,21 +3,24 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 
+app.use(express.json());
+app.use(cookieParser());
 
 
 const authRouter = require("./routes/auth")
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
 
 
-
-app.use(express.json());
-app.use(cookieParser());
 
 app.use("/",authRouter);
 app.use("/", profileRouter);
 app.use("/",requestRouter);
+app.use("/",userRouter);
+ 
+
 
 connectDB()
   .then(() => {
